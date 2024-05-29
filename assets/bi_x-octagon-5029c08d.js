eb438a37816618c6d93377cec89c6d5ce4a779de
polaris-on <104370675+polaris-on@users.noreply.github.com>
2024-05-29 14:54:59 +0000

    Deploying to gh-pages from @ polaris-on/goit-js-hw-10@d18a68f56243d84e3fd061289439f18aa27506f8 🚀

---
+++ assets/bi_x-octagon-5029c08d.js
@@ -0,0 +1,2 @@
+const s="/goit-js-hw-10/assets/check-circle-a2efc8ff.svg",c="/goit-js-hw-10/assets/bi_x-octagon-976d29f8.svg";export{s as a,c as i};
+//# sourceMappingURL=bi_x-octagon-5029c08d.js.map
--- assets/check-circle-594a3bc7.js
+++
@@ -1,2 +0,0 @@
-const c="/goit-js-hw-10/assets/check-circle-a2efc8ff.svg";export{c as i};
-//# sourceMappingURL=check-circle-594a3bc7.js.map
